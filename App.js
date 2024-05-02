a15e75ca1add144e4785c5409627198698956b59
unknown <osiadmin@OSI-L-1490.osius.com>
2024-05-02 17:44:22 +0530

    Namaste React Practice

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement('h1',{id:'heading'},'I am h1 from React');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+console.log(heading);
